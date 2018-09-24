0929801729aca167727995373baf1322566b3b6f
Till Neunast <tilln@users.noreply.github.com>
2018-09-24 19:54:06 +1200

    Fix type parameter

--- vars/jmeterPlot.groovy
+++ vars/jmeterPlot.groovy
@@ -4,7 +4,7 @@ def call(parameters = [:]) {
     def inputs = findFiles(glob: parameters.inputs ?: '**/*.csv')
     def output = parameters.output ?: 'index.html'
     def type = parameters.type ?: 'scatter'
-    def html = ~/(?i)static/ === type ? StaticJmeterPlotter.generateHtml(inputs, parameters)
+    def html = type =~ /(?i)static/ ? StaticJmeterPlotter.generateHtml(inputs, parameters)
         : JmeterPlotter.generateHtml(inputs, type.toLowerCase(), parameters.hover ?: '')
     writeFile text: html, file: output
 }
